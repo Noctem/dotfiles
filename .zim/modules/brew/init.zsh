d06e00a639c41f06fb0fde19d0dc686d339f56e9
David Christenson <mail@noctem.xyz>
2019-10-08 01:13:13 -0600

    Ensure that homebrew builds with -march=native

--- .zim/modules/brew/init.zsh
+++ .zim/modules/brew/init.zsh
@@ -6,16 +6,18 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_FORCE_BREWED_CURL=1
 export HOMEBREW_FORCE_BREWED_GIT=1
 export HOMEBREW_INSTALL_CLEANUP=1
+export HOMEBREW_DEVELOPER=1
+export HOMEBREW_NO_AUTO_UPDATE=1
 
 # build from source (-s)
 alias bi='brew install -s'
+alias bupd='brew update; [[ $(git --work-tree=/usr/local/Homebrew --git-dir=/usr/local/Homebrew/.git status --porcelain Library/Homebrew/extend/ENV/shared.rb) != " M Library/Homebrew/extend/ENV/shared.rb" ]] && (pushd /usr/local/Homebrew; git reset --hard; patch --no-backup-if-mismatch -p1 -i /Users/david/.dotother/homebrew-native.diff; popd)'
 alias bupg='brew upgrade -s'
 alias br='brew reinstall -s'
-alias buu='brew update && brew upgrade -s'
-alias buuf='brew update && brew upgrade -s --fetch-HEAD'
-alias buuc='brew update && brew upgrade -s --fetch-HEAD && brew cask upgrade'
+alias buu='bupd; brew upgrade -s'
+alias buuf='bupd; brew upgrade -s --fetch-HEAD'
+alias buuc='bupd; brew upgrade -s --fetch-HEAD && brew cu -a'
 
-alias bupd='brew update'
 alias bun='brew uninstall'
 alias binf='brew info'
 
