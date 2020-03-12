f2a5ebfe67be13f61897a150d4e9c17e9d4742ed
David Christenson <mail@noctem.xyz>
2020-03-12 13:31:41 -0600

    Tweak environment build flags

--- .zim/modules/pref/init.zsh
+++ .zim/modules/pref/init.zsh
@@ -4,15 +4,17 @@
 
 export PATH="/usr/local/opt/python/libexec/bin:/usr/local/opt/ccache/libexec:${HOME}/bin:/usr/local/bin/Scripts:${HOME}/Library/Python/3.8/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export MACOSX_DEPLOYMENT_TARGET=10.15
-export CFLAGS='-march=native -pipe -flto'
-export CXXFLAGS="${CFLAGS}"
-export OBJCFLAGS="${CFLAGS}"
-export OBJCXXFLAGS="${CFLAGS}"
-export LDFLAGS="-flto"
+export CFLAGS='-march=native -pipe -flto=thin -O3'
+export CXXFLAGS="$CFLAGS"
+export OBJCFLAGS="$CFLAGS"
+export OBJCXXFLAGS="$CFLAGS"
+export LDFLAGS="$CFLAGS"
+export RUSTFLAGS="-C target-cpu=native"
 
 # use rmate if connected via SSH, else mate
 [[ $SSH_TTY ]] && export EDITOR='rmate -w' || export EDITOR='mate -w'
 
+alias clang-format=/usr/local/opt/llvm/bin/clang-format
 alias clang-check=/usr/local/opt/llvm/bin/clang-check
 alias clang-tidy=/usr/local/opt/llvm/bin/clang-tidy
 
