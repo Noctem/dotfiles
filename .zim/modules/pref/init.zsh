0617bf734b626592bb10470966f1c86c4e36a7c7
David Christenson <mail@noctem.xyz>
2022-04-11 17:55:41 -0600

    add string encryption functions

--- .zim/modules/pref/init.zsh
+++ .zim/modules/pref/init.zsh
@@ -2,6 +2,7 @@
 # exports some variables and sets some aliases
 #
 
+export LANG=en_US.UTF-8
 export PATH="${HOME}/bin:${HOME}/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
 export CFLAGS='-march=native -pipe -flto'
 export CXXFLAGS="$CFLAGS"
@@ -15,6 +16,14 @@ alias config='git --git-dir="${HOME}/.cfg" --work-tree="$HOME"'
 alias l='exa -lahm'
 alias pac='pacman'
 alias spac='sudo pacman'
+
+function encryptString() {
+	echo "$1" | openssl enc -aes-256-cbc -md sha512 -a -pbkdf2 -iter 100000 -salt -pass pass:"$2"
+}
+
+function decryptString() {
+	echo "$1" | openssl enc -aes-256-cbc -md sha512 -a -d -pbkdf2 -iter 100000 -salt -pass pass:"$2"
+}
 
 # Ctrl-Left and Ctrl-Right to move between words
 bindkey "^[[1;5C" forward-word
